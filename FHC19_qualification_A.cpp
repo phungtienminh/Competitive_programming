f3fae2aed0538d4d56cc1244fa54cc138a9e1f32
Phung Tien Minh <37219152+phungtienminh@users.noreply.github.com>
2019-06-18 00:16:09 +0700

    implementation

---
+++ FHC19_qualification_A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+string s;
+
+int main(){
+    //freopen("leapfrog_ch_.txt", "r", stdin);
+    //freopen("contestant.out", "w", stdout);
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> t;
+    for (int T = 1; T <= t; T++) {
+        cout << "Case #" << T << ": ";
+        cin >> s;
+        int cnt = 0;
+        for (int j = 0; j < s.length(); j++) cnt += (s[j] == 'B');
+        cout << (cnt >= s.length() / 2 && cnt != s.length() - 1 ? "Y\n" : "N\n");
+    }
+    return 0;
+}
